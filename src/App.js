09f64bed58f90b3ea5593e3f84cc632d52aa2a6c
michaelrichey <michael@tofugu.com>
2021-06-17 10:28:24 -0400

    handle checkbox click without state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const todoList = [
+    { id: 1, text: "Define character relationships", done: false },
+    { id: 2, text: "Define character goals", done: false },
+    { id: 3, text: "Map plot", done: false },
+  ];
+
+  React.useEffect(() => {});
+
+  function handleCheckbox(index) {
+    todoList[index].done = !todoList[index].done;
+    console.log("fire");
+    console.log(todoList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>What would you like to get done today?</h1>
+      {todoList.map((todo, index) => {
+        return (
+          <React.Fragment>
+            <ul>
+              <li>
+                <input type="checkbox" onClick={() => handleCheckbox(index)} />
+                {todo.text}
+              </li>
+            </ul>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
